71f053c20272b50495b41125d402296806d29693
nonnumbered <lufx432110@gmail.com>
2025-08-21 20:15:38 -0400

    add file

---
+++ FiveAndSeventySix.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FiveAndSeventySix {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = input.nextInt();
+    if (!(num >= 5 && num <= 76)) {
+        System.out.println("True");
+
+    } else {
+        System.out.println("False");
+        
+    }
+}
+}
